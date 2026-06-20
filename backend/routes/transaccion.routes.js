3323f27934ef44ae922f7eccdca5d0a9bb48491f
JulianScipioni06 <scipionijulian@gmail.com>
2026-06-20 14:19:18 -0300

    archivo rutas

---
+++ backend/routes/transaccion.routes.js
@@ -0,0 +1,7 @@
+const { Router } = require('express');
+const router = Router();
+
+// Acá los chicos del equipo van a ir metiendo los endpoints
+// router.get('/', obtenerTodos);
+
+module.exports = router;
